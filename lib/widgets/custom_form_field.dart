ed54be01c135a9c68f087a88212a12b9450e7efc
yohan <yovirinc@gmail.com>
2025-12-02 19:22:17 +0100

    obscure icon fixed

--- lib/widgets/custom_form_field.dart
+++ lib/widgets/custom_form_field.dart
@@ -1,11 +1,15 @@
 import 'package:classlog/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/fa.dart';
 
-class CustomFormField extends StatelessWidget {
+class CustomFormField extends StatefulWidget {
   final String labelText;
   final TextEditingController controller;
   final TextInputType? keyboardType;
   final String? Function(String?)? validator;
+  final Iconify? suffixIcon;
+  final bool isPassword;
 
   const CustomFormField({
     super.key,
@@ -13,7 +17,23 @@ class CustomFormField extends StatelessWidget {
     required this.controller,
     this.keyboardType = TextInputType.text,
     required this.validator,
+    this.suffixIcon,
+    this.isPassword = false,
+    required bool obscureText,
   });
+
+  @override
+  State<CustomFormField> createState() => _CustomFormFieldState();
+}
+
+class _CustomFormFieldState extends State<CustomFormField> {
+  bool obscureText = false; // ✅ 바로 초기화
+
+  @override
+  void initState() {
+    super.initState();
+    obscureText = widget.isPassword;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +41,7 @@ class CustomFormField extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          labelText,
+          widget.labelText,
           style: TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.w600,
@@ -31,14 +51,31 @@ class CustomFormField extends StatelessWidget {
           height: 8,
         ),
         TextFormField(
-          controller: controller,
+          controller: widget.controller,
+          obscureText: obscureText,
           decoration: InputDecoration(
-              border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: lineColor)),
-              focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: mainColor))),
+            border: OutlineInputBorder(
+                borderSide: const BorderSide(color: lineColor)),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: mainColor),
+            ),
+            suffixIcon: widget.isPassword
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscureText = !obscureText;
+                      });
+                    },
+                    icon: Iconify(
+                      obscureText ? Fa.eye : Fa.eye_slash,
+                      size: 18,
+                      color: textSecondaryColor,
+                    ),
+                  )
+                : null,
+          ),
           keyboardType: TextInputType.emailAddress,
-          validator: validator,
+          validator: widget.validator,
         ),
       ],
     );
