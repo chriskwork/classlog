42edebe49316c346778c2e0cc86fd4bd5de4741f
chris k <yovirinc@gmail.com>
2025-12-10 02:53:46 +0100

    Calendar setup

--- lib/models/calendar_event.dart
+++ lib/models/calendar_event.dart
@@ -5,12 +5,19 @@ enum EventType {
   proyecto,
 }
 
+enum AttendanceStatus {
+  presente,
+  tarde,
+  ausente,
+}
+
 class CalendarEvent {
   final DateTime date;
   final EventType type;
-  final String? status; // presente, tarde, ausente
+  final AttendanceStatus? status; // presente, tarde, ausente
   final String? courseId;
   final String? courseName;
+  final String? timeRange;
 
   CalendarEvent({
     required this.date,
@@ -18,16 +25,20 @@ class CalendarEvent {
     this.status,
     this.courseId,
     this.courseName,
+    this.timeRange,
   });
 
   // TODO: API + later
-  // factory CalendarEvent.fromJson(Map<String, dynamic> json) {
-  //   return CalendarEvent(
-  //     date: DateTime.parse(json['date']),
-  //     type: EventType.values.byName(json['type']),
-  //     status: json['status'],
-  //     courseId: json['course_id'],
-  //     courseName: json['course_name'],
-  //   );
-  // }
+  factory CalendarEvent.fromJson(Map<String, dynamic> json) {
+    return CalendarEvent(
+      date: DateTime.parse(json['date']),
+      type: EventType.values.byName(json['type']),
+      status: json['status'] != null
+          ? AttendanceStatus.values.byName(json['status'])
+          : null,
+      courseId: json['course_id'],
+      courseName: json['course_name'],
+      timeRange: json['time_range'],
+    );
+  }
 }
