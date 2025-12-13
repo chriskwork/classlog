50968919c7b565f480cf3672fe06502ee9da3c88
chris k <yovirinc@gmail.com>
2025-12-13 02:23:05 +0100

    Course card

--- lib/screens/my_courses_screen.dart
+++ lib/screens/my_courses_screen.dart
@@ -1,5 +1,6 @@
 import 'package:classlog/data/mock_course.dart';
 import 'package:classlog/screens/course_detail_sheet.dart';
+import 'package:classlog/theme/colors.dart';
 import 'package:classlog/theme/settings.dart';
 import 'package:classlog/widgets/course_card.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +14,12 @@ class MyCoursesScreen extends StatelessWidget {
       context: context,
       isScrollControlled: true,
       backgroundColor: Colors.transparent,
-      builder: (context) => CourseDetailSheet(course: course),
+      builder: (context) => GestureDetector(
+        onTap: () => Navigator.pop(context),
+        behavior: HitTestBehavior.opaque,
+        child: GestureDetector(
+            onTap: () {}, child: CourseDetailSheet(course: course)),
+      ),
     );
   }
 
@@ -25,14 +31,25 @@ class MyCoursesScreen extends StatelessWidget {
         separatorBuilder: (_, __) => const SizedBox(height: 12),
         itemBuilder: (context, index) {
           final course = courses[index];
-          return CourseCard(
-            courseName: course['name'],
-            time: course['time'],
-            status: course['status'],
-            icon: course['icon'],
-            iconColor: course['iconColor'],
-            iconBgColor: course['iconBgColor'],
-            onTap: () => _showCourseDetail(context, course),
+          return DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: lineColor,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+              child: CourseCard(
+                courseName: course['name'],
+                time: course['time'],
+                icon: course['icon'],
+                iconColor: course['iconColor'],
+                iconBgColor: course['iconBgColor'],
+                onTap: () => _showCourseDetail(context, course),
+              ),
+            ),
           );
         });
   }
--- lib/widgets/course_card.dart
+++ lib/widgets/course_card.dart
@@ -8,14 +8,14 @@ class CourseCard extends StatelessWidget {
   final Color iconBgColor;
   final String courseName;
   final String time;
-  final String status;
+  final String? status;
   final VoidCallback? onTap;
 
   const CourseCard({
     super.key,
     required this.courseName,
     required this.time,
-    required this.status,
+    this.status,
     required this.icon,
     required this.iconColor,
     required this.iconBgColor,
@@ -24,66 +24,71 @@ class CourseCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      spacing: Gaps.sm,
-      children: [
-        DecoratedBox(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: iconBgColor,
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: Icon(
-              icon,
-              color: iconColor,
-              size: 30,
+    return GestureDetector(
+      onTap: onTap,
+      behavior: HitTestBehavior.opaque,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        spacing: Gaps.sm,
+        children: [
+          DecoratedBox(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: iconBgColor,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Icon(
+                icon,
+                color: iconColor,
+                size: 30,
+              ),
             ),
           ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                courseName, // DB - class title
-                style: Theme.of(context).textTheme.bodyMedium,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              ),
-              Text(
-                time, // DB - time here
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      color: secondaryColor,
-                    ),
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          width: 4,
-        ),
-        DecoratedBox(
-          decoration: BoxDecoration(
-            color: switch (status) {
-              "Presente" => Colors.greenAccent[100],
-              "Ausencia" => Colors.red[200],
-              _ => Colors.grey[200],
-            },
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              status,
-              style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: secondaryColor,
-                  ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  courseName, // DB - class title
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+                Text(
+                  time, // DB - time here
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: secondaryColor,
+                      ),
+                ),
+              ],
             ),
           ),
-        ),
-      ],
+          SizedBox(
+            width: 4,
+          ),
+          if (status != null)
+            DecoratedBox(
+              decoration: BoxDecoration(
+                color: switch (status) {
+                  "Presente" => Colors.greenAccent[100],
+                  "Ausencia" => Colors.red[200],
+                  _ => Colors.grey[200],
+                },
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  status!,
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                        color: secondaryColor,
+                      ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
