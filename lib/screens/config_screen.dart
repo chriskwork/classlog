ba1f71d484ff170a85e76a5d4716a0cd64e35d47
chris k <yovirinc@gmail.com>
2025-12-21 02:02:19 +0100

    working..

--- lib/screens/config_screen.dart
+++ lib/screens/config_screen.dart
@@ -1,4 +1,5 @@
 import 'package:classlog/theme/colors.dart';
+import 'package:classlog/theme/settings.dart';
 import 'package:flutter/material.dart';
 
 class ConfigScreen extends StatefulWidget {
@@ -11,98 +12,102 @@ class ConfigScreen extends StatefulWidget {
 class _ConfigScreenState extends State<ConfigScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 20,
-        ),
-
-        // Profile image
-        Center(
-            child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withAlpha(80),
-                    blurRadius: 10,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: CircleAvatar(
-                radius: 60,
-                backgroundColor: Colors.white,
-                child: CircleAvatar(
-                  radius: 56,
-                  backgroundImage: AssetImage('assets/profile.png'),
-                ),
-              ),
-            ),
-
-            // Edit icon
-            Positioned(
-              right: 0,
-              bottom: 0,
-              child: Container(
-                padding: const EdgeInsets.all(6),
-                decoration: BoxDecoration(
-                  color: mainColor, // 파란색
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.white, width: 2),
-                ),
-                child: Icon(
-                  Icons.edit,
-                  size: 20,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        )),
-        const SizedBox(height: 20),
-        Text(
-          // TODO: user name from DB
-          "Yohan Kim",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.all(Gaps.lg),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
           ),
-        ),
 
-        const SizedBox(height: 40),
-
-        // Config options
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-          elevation: 0,
-          child: Column(
+          // Profile image
+          Center(
+              child: Stack(
             children: [
-              // Perfil
-              ListTile(
-                leading: Icon(Icons.abc),
-                title: Text("Editar Perfil"),
-                trailing: Icon(Icons.chevron_right),
-                onTap: () {},
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(80),
+                      blurRadius: 10,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    radius: 56,
+                    backgroundImage: AssetImage('assets/profile.png'),
+                  ),
+                ),
               ),
 
-              const Divider(height: 1, color: lineColor),
-
-              // Seguridad
-              ListTile(
-                leading: Icon(Icons.abc),
-                title: Text("Seguridad"),
-                trailing: Icon(Icons.chevron_right),
-                onTap: () {},
-              )
+              // Edit icon
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    color: mainColor, // 파란색
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: Icon(
+                    Icons.edit,
+                    size: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ],
+          )),
+          const SizedBox(height: 20),
+          Text(
+            // TODO: user name from DB
+            "Yohan Kim",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        )
-      ],
+
+          const SizedBox(height: 40),
+
+          // Config options
+          Card(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: lineColor, width: 1.0),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            elevation: 0,
+            child: Column(
+              children: [
+                // Perfil
+                ListTile(
+                  leading: Icon(Icons.abc),
+                  title: Text("Editar Perfil"),
+                  trailing: Icon(Icons.chevron_right),
+                  onTap: () {},
+                ),
+
+                const Divider(height: 1, color: lineColor),
+
+                // Seguridad
+                ListTile(
+                  leading: Icon(Icons.abc),
+                  title: Text("Seguridad"),
+                  trailing: Icon(Icons.chevron_right),
+                  onTap: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
